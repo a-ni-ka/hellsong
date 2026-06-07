dc4b0b4b0e8568eeb0c9ea87256a1cbf8a49d50e
DizTizz <thiespetersen01@hotmail.com>
2026-06-07 14:48:08 +0200

    Updatetd Godot, Character can move

---
+++ hellsong/Player/main_character.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+
+func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_axis("mvm_left", "mvm_right")
+	var direction_y := Input.get_axis("mvm_up", "mvm_down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
